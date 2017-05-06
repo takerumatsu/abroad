c39c7cb8ac351c61090f06cae9ba114785f5ff0d
takeru <takeru.matsukawa@gmail.com>
2017-05-06 23:50:16 +0000

    top page country

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  get 'toppages/europe', to: 'toppages#europe'
+  get 'toppages/asia', to: 'toppages#asia'
+  get 'toppages/america', to: 'toppages#america'
 end
